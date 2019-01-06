0e24a368ff2893c8d26a5532182d85d82569d7b8
dominic <dolgushin111@yandex.ru>
2019-01-06 22:26:46 +0700

    made frontend CRUD categories and positions

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -2,12 +2,26 @@ import {ElementRef} from "@angular/core";
 
 declare var M
 
+export interface MaterialInstance {
+  open?(): void
+  close?(): void
+  destroy?(): void
+}
+
 export class MaterialService {
   static toast(message: string) {
     M.toast({html: message})
   }
 
-  static initializeFloatingButton(ref: ElementRef){
+  static initializeFloatingButton(ref: ElementRef) {
     M.FloatingActionButton.init(ref.nativeElement)
+  }
+
+  static updateTextInputs() {
+    M.updateTextFields()
+  }
+
+  static initModal(ref: ElementRef) {
+    return M.Modal.init(ref.nativeElement)
   }
 }
