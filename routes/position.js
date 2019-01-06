7ccbd0cb93d49a217981ad655202a5059a4e33d2
dominic <dolgushin111@yandex.ru>
2019-01-06 20:35:54 +0700

    fix type request

--- routes/position.js
+++ routes/position.js
@@ -7,7 +7,7 @@ const auth = passport.authenticate('jwt', {session: false});
 
 router.get('/:categoryId', auth, controller.getByCategoryId);
 router.post('/', auth, controller.create);
-router.post('/:id', auth, controller.update);
+router.patch('/:id', auth, controller.update);
 router.delete('/:id', auth, controller.remove);
 
 
