02bec7f728f659f3fe6c6c663ba822d9ccd4c4e3
dominic <dolgushin111@yandex.ru>
2019-01-07 16:06:45 +0700

    fix async function in method create

--- controllers/order.js
+++ controllers/order.js
@@ -36,7 +36,7 @@ module.exports.create = async (req, res)=>{
     try {
         const lastOrder = await Order.findOne({user: req.user.id}).sort({date: -1});
         const maxOrder = lastOrder ? lastOrder.order : 0;
-        const  order = new Order({
+        const  order = await new Order({
             list: req.body.list,
             user: req.user.id,
             order: maxOrder+1
