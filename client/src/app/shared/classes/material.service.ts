c71a92e068240a01e897427a3f9a71e2da247739
dominic <dolgushin111@yandex.ru>
2019-01-08 02:14:03 +0700

    made frontend history page orders

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -4,8 +4,14 @@ declare var M
 
 export interface MaterialInstance {
   open?(): void
+
   close?(): void
+
   destroy?(): void
+}
+
+export interface MaterialDatepicker extends MaterialInstance{
+  date?: Date
 }
 
 export class MaterialService {
@@ -21,7 +27,19 @@ export class MaterialService {
     M.updateTextFields()
   }
 
-  static initModal(ref: ElementRef) {
+  static initModal(ref: ElementRef): MaterialInstance {
     return M.Modal.init(ref.nativeElement)
+  }
+
+  static initTooltip(ref: ElementRef): MaterialInstance {
+    return M.Tooltip.init(ref.nativeElement)
+  }
+
+  static initDatepicker(ref: ElementRef, onClose: () => void): MaterialDatepicker {
+    return M.Datepicker.init(ref.nativeElement, {
+      format: 'dd.mm.yyyy',
+      showClearBtn: true,
+      onClose
+    })
   }
 }
